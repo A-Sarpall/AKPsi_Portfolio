11615ee3a7636f105366127127c0bd142ed44e09
Ahan Saripalli <frootyloops44@gmail.com>
2025-11-23 22:00:48 -0600

    nov 2025 update

--- ahan/src/components/Projects.js
+++ ahan/src/components/Projects.js
@@ -9,66 +9,82 @@ import PsiFlow from '../assets/PsiFlow.png';
 import GridironGuide from '../assets/GridironGuide.png';
 import RatioAdjuster from '../assets/RatioAdjuster.png';
 import Dagger from '../assets/Dagger.png';
+import prodplex from '..assets/prodplex.png';
+import phoniverse from '../assets/phoniverse.png';
 
 const projects = [
   {
     id: 1,
+    title: 'ProdPlex',
+    description: 'Agentic AI platform for PMs to iterate faster and generate a cohesive Product Requirement Document for all parts of the Product Life Cycle',
+    imageUrl: prodplex,
+    githubUrl: 'https://devpost.com/software/prodify-94vbpk'
+  },
+  {
+    id: 2,
+    title: 'Phoniverse',
+    description: 'Gamified Speech Therapy utilizing AI to help people overcome speech impediments by reciting excersises based on their own voice ',
+    imageUrl: phoniverse,
+    githubUrl: 'https://devpost.com/software/phoniverse'
+  },
+  {
+    id: 3,
     title: 'Nomad',
     description: 'Website for users to find remote workspaces. Presented to the brothers of the Mu Rho chapter of Alpha Kappa Psi during Court of Honor.',
     imageUrl: nomad,
     githubUrl: 'https://nomad-green.vercel.app/'
   },
   {
-    id: 2,
+    id: 4,
     title: 'SoleSwitch',
     description: 'Slideshow of a hypothetical new product that allows users to switch between different soles of their shoes with ease. Presented to the brothers of the Mu Rho chapter of Alpha Kappa Psi during MidCourt.',
     imageUrl: SoleSwitch,
     githubUrl: 'https://www.canva.com/design/DAGis6LgZ9g/NhTBUdKIlHmozzzBWw9k0g/edit?utm_content=DAGis6LgZ9g&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton'
   },
   {
-    id: 3,
+    id: 5,
     title: 'Shakespearen Monkeys',
     description: "Generates realistic English text by analyzing character sequence patterns from Shakespeare's 'The Merchant of Venice' and recreating them using a probabilistic model. It also demonstrates custom implementations of the Map ADT using both a binary search tree and a hash table.",
     imageUrl: ShakespearenMonkeys,
     githubUrl: 'https://github.com/A-Sarpall/DSA_Project'
   },
   {
-    id: 4,
+    id: 6,
     title: 'Dagger',
     description: 'Real-time credit card transaction analyzer used to detect fraudulent transactions based on behavioral analysis',
     imageUrl: Dagger,
     githubUrl: 'https://github.com/A-Sarpall/Dagger'
   },
   {
-    id: 5,
+    id: 7,
     title: 'MoodSing',
     description: 'Facial recognition software powered by AI to detect your mood based on your current face structure and change the song if your mood is negative.',
     imageUrl: MoodSing,
     githubUrl: 'https://github.com/A-Sarpall/MoodSing'
   },
   {
-    id: 6,
+    id: 8,
     title: 'PsiFlow',
     description: 'Financial dashboard for UTD Alpha Kappa Psi Fraternity to view and edit budgets, and create custom Stripe payment links for pledges, brothers, and corporate sponsors to pay for services/dues.',
     imageUrl: PsiFlow,
     githubUrl: 'https://github.com/a-sarpall/psiflow'
   },
   {
-    id: 7,
+    id: 9,
     title: 'GridironGuide',
     description: 'Interactive platform to help football fans easily track and visualize the upcoming schedule for their favorite teams and rivals.',
     imageUrl: GridironGuide,
     githubUrl: 'https://gridironguide.netlify.app/'
   },
   {
-    id: 8,
+    id: 10,
     title: 'RatioAdjuster',
     description: "Users can manage their Instagram following ratio by seeing which accounts don't follow them back.",
     imageUrl: RatioAdjuster,
     githubUrl: 'https://ratioadjuster.streamlit.app/'
   },
   {
-    id: 9,
+    id: 11,
     title: 'Under Construction',
     description: "More projects coming soon!",
     imageUrl: construction
